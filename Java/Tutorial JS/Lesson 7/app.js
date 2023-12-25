ea7d8a576372bad0e3cd0f600c0337316ed30305
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-25 16:09:07 +0500

    Array

---
+++ Java/Tutorial JS/Lesson 7/app.js
@@ -0,0 +1,15 @@
+
+
+const mehmon1='Azizbek'
+const mehmon2='Ali'
+const mehmon3='Vali'
+const mehmon4='Asror'
+const mehmon5='Diyor'
+
+// array - massiv
+
+const mehmonlar=['Azizbek','Ali','Vali','Diyor','Asror']
+
+
+mehmonlar.push('Davlatbek');
+console.log(mehmonlar)
