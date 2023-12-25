19dbf941d3281c0b8596f1ae9c915fc51214ef89
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-25 15:26:49 +0500

    Numbers

---
+++ Java/Tutorial JS/Lesson5/app.js
@@ -0,0 +1,17 @@
+
+
+let radius=10;
+const pi=3.14;
+console.log(radius,pi);
+
+// math operators 
+
+console.log(10 / 2)
+
+let result=radius%3
+
+console.log(result)
+
+console.log(5/'salom')
+
+console.log('Qaale '+10+' likes')
