f14196a279596ce03cf22992ef898f4737805b92
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-25 11:05:51 +0500

    Lesson 2

---
+++ Java/Tutorial JS/Lesson2/app.js
@@ -0,0 +1,18 @@
+
+console.log(1);
+console.log(2);
+let ism='Azizbek';
+let yosh=24;
+console.log(ism,yosh);
+
+yosh=25;
+console.log(ism,yosh);
+
+const familiya='Rahmatullayev';
+
+console.log(familiya);
+
+
+var joy ='Fargona';
+joy = 'Fargona tumani';
+console.log(joy);
