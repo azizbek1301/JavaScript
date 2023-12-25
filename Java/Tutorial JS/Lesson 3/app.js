9c5aa7b9331f0f964ad4c317033a4cec306e8171
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-25 12:26:12 +0500

    Lesson3 String operations

---
+++ Java/Tutorial JS/Lesson 3/app.js
@@ -0,0 +1,18 @@
+
+
+let firstName='Ahror';
+let lastName='Soliyev';
+
+let fullName=firstName+' '+lastName;
+console.log(fullName);
+
+console.log(fullName[2]);
+
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+
+console.log(fullName.toLowerCase());
+
+let index=fullName.indexOf('y');
+console.log(index);
