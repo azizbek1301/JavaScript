b42b149c19cbf706cd014c7dbaaa266c2347aeec
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-25 15:42:43 +0500

    String lar davomi

---
+++ Java/Tutorial JS/Lesson6/app.js
@@ -0,0 +1,15 @@
+const title='Rastamojka qachon ochiladi biladiganlar aytib yuborilar iltimos'
+
+const author="Lag'mon markazi"
+
+const likes=35000000
+
+let result="bu post nomi " + title + ' ' + author + ' tomonida yozilgan. ' + ' va bu postda '
++ likes + ' bor'
+
+console.log(result)
+
+
+let result2=`Bu post nomi ${title}, ${author} tomonidan yozilgan va bu postda ${likes} ta likelar bor`
+console.log(result2)
+
